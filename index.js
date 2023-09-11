b51c3bac2113cb90b8660275d719e7c52fa39458
seasons986 <annalin12345@gmail.com>
2023-09-11 10:37:27 -0400

    calculate and leave some space in the center for godly business

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 window.addEventListener("load", () => {
-  function getRandomIntNotCenter(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-  }
-
-  const container = document.getElementById("container");
-  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   const wOffset = (window.innerWidth/15);
   const w = window.innerWidth - wOffset;
   const hOffset = (window.innerHeight/20);
   const h = window.innerHeight - hOffset;
-  console.log({w,wOffset,h,hOffset})
+  const wHalf = (w/2)+wOffset;
+  const hHalf = (h/2)+hOffset;
+  const r = 50;
+  function getRandomIntNotCenter(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    let num = Math.floor(Math.random() * (max - min) + min);
+    if ((num > (wHalf-r) && num <= (wHalf+r)) || (num > (hHalf-r) && num <= (hHalf+r))) {
+      num+=(r+(Math.random()*5));
+    }
+    return num // The maximum is exclusive and the minimum is inclusive
+  }
+  
+  const container = document.getElementById("container");
+  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
   function display(ch) {
     const span = document.createElement("span");
